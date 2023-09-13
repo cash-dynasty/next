da85ec7610c0924a40d71ee2ffd0798f20e58905
Bartosz Jaworowicz <68512578+Demorhul@users.noreply.github.com>
2023-09-13 15:12:29 +0200

    Co-authored-by: Marcin Wiatrak <marcin-wiatrak@users.noreply.github.com> (#2)

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,8 @@
+export const Button = ({ label }) => {
+    return (
+
+        <button className="bg-primary-50 text-tertiary-75 px-[30px] py-[14px] border-2 font-goldman border-1 border-primary-100 ">
+            {label}
+        </button>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,30 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      'goldman': ['Goldman']
+    },
+
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          100: 'rgba(255, 110, 32, 1)',
+          75: 'rgba(255, 110, 32, .75)',
+          50: 'rgba(255, 110, 32, .5)',
+          25: 'rgba(255, 110, 32, .25)',
+        },
+        secondary: {
+          100: 'rgba(24 , 53, 58, 1)',
+          75: 'rgba(24 , 53, 58, .75)',
+          50: 'rgba(24 , 53, 58, .5)',
+          25: 'rgba(24 , 53, 58, .25)',
+        },
+        tertiary: {
+          100: 'rgba(204 , 204, 204, 1)',
+          75: 'rgba(204 , 204, 204, .75)',
+          50: 'rgba(204 , 204, 204, .5)',
+          25: 'rgba(204 , 204, 204, .25)',
+        }
       },
     },
   },
