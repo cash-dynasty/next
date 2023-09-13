4251c7ced0ecc2d8aba5346e2ce462ec0bd0f6ec
Bartosz Jaworowicz <68512578+Demorhul@users.noreply.github.com>
2023-09-13 20:35:09 +0200

    Create imput component, button bug fix (#3)
    * Create imput components.
    
    * Update src/app/page.tsx
    
    Co-authored-by: Marcin Wiatrak <marcin.wiatrak.95@gmail.com>
    
    ---------
    
    Co-authored-by: Marcin Wiatrak <marcin.wiatrak.95@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
+'use client'
 import Image from 'next/image'
 import { Button } from '@atoms/Button'
+import { TextInput } from '@atoms/TextInput'
+import { useState } from 'react'
 
 export default function Home() {
+  const [input, setInput] = useState<string | null>(null)
   return (
-    <div className='h-[100vh] bg-black'>
+    <div className="h-[100vh] bg-black">
       <Button label="ATAKUJ" />
+      <br />
+      <br />
+      <TextInput
+        leftIcon
+        placeholder="Username"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
     </div>
   )
 }
