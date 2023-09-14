b8b3f831805abe8483e496e9de2bdd8bc140d93c
JimmyPagge <81492223+JimmyPagge@users.noreply.github.com>
2023-09-14 21:59:43 +0200

    files format (#7)
    * files format
    
    * added vscode to gitignore
    
    * Update src/app/api/users/route.tsx
    
    Co-authored-by: Marcin Wiatrak <marcin.wiatrak.95@gmail.com>
    
    ---------
    
    Co-authored-by: Marcin Wiatrak <marcin.wiatrak.95@gmail.com>

--- src/components/Molecules/MenuBar/MenuItem/MenuItem.tsx
+++ src/components/Molecules/MenuBar/MenuItem/MenuItem.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
+import React from 'react'
 
 export const MenuItem = () => {
-    return (
-        <div>
-            Menu item
-        </div>
-    );
-};
+  return <div>Menu item</div>
+}
