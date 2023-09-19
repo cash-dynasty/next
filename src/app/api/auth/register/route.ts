b26db72208fd777d8a063ebc3d8510cb8933867d
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2023-09-19 22:54:04 +0200

    fix

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,17 +1,24 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 import bcrypt from 'bcrypt'
 import { prisma } from '@/utils/db'
+import { Prisma } from '@prisma/client'
 
 export async function POST(request: Request) {
   const { username, password, email } = await request.json()
-  console.log('1', username, password, email)
-  console.time('hash')
-  const passwordHash = await bcrypt.hash(password, 5)
-  console.timeEnd('hash')
-  console.log('2')
-  const data = await prisma.user.create({
-    data: { nickname: username, password: passwordHash, email },
-  })
-  console.log('data::::', data)
+  const passwordHash = await bcrypt.hash(password, 10)
+  console.log(username, password, email)
+  try {
+    await prisma.user.create({
+      data: { username, password: passwordHash, email },
+    })
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.log(error.code, error.message)
+      return NextResponse.json(
+        { status: 'fail', error: error.code },
+        { status: 409 },
+      )
+    }
+  }
   return NextResponse.json({ status: 'ok' })
 }
