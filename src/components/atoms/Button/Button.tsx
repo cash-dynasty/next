35119f5f0a53843e543b4385d12abdd1f3605251
Marcin Wiatrak <marcin.wiatrak.95gmail.com>
2023-09-19 18:11:08 +0200

    resend setup

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,10 +1,14 @@
 type ButtonProps = {
   label: string
+  onClick?: () => void
 }
 
-export const Button = ({ label }: ButtonProps) => {
+export const Button = ({ label, onClick }: ButtonProps) => {
   return (
-    <button className="border-2 bg-primary-50 text-tertiary-75 px-[30px] py-[14px] font-goldman border-primary-100 hover:bg-primary-75 hover:text-tertiary-100">
+    <button
+      className="border-2 bg-primary-50 text-tertiary-75 px-[30px] py-[14px] font-goldman border-primary-100 hover:bg-primary-75 hover:text-tertiary-100"
+      onClick={onClick}
+    >
       {label}
     </button>
   )
