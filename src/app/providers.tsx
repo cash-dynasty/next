33d94a0c5eac77c07f8bb1c2a58fb0867b6be4bf
Marcin Wiatrak <marcin.wiatrak.95@gmail.com>
2023-09-23 20:50:41 +0200

    strict recaptcha to only form pages

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,29 +4,15 @@ import { store } from '@/store/store'
 import { SessionProvider } from 'next-auth/react'
 import React from 'react'
 import { Provider } from 'react-redux'
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 
 type Props = {
   children?: React.ReactNode
 }
 
 export const Providers = ({ children }: Props) => {
-  // console.log('env1', process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY)
   return (
     <SessionProvider>
-      <Provider store={store}>
-        <GoogleReCaptchaProvider
-          reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? ''}
-          scriptProps={{
-            async: false,
-            defer: false,
-            appendTo: 'body',
-            nonce: undefined,
-          }}
-        >
-          {children}
-        </GoogleReCaptchaProvider>
-      </Provider>
+      <Provider store={store}>{children}</Provider>
     </SessionProvider>
   )
 }
