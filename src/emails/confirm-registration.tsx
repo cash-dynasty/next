838c734eedf3b0d3217f000c79136aee9bca3eb7
Marcin Wiatrak <marcin.wiatrak.95@gmail.com>
2023-09-23 22:41:01 +0200

    feature/CD-21-React-email (#14)
    * CD-21 setup react-email
    
    * CD-21 rename function

---
+++ src/emails/confirm-registration.tsx
@@ -0,0 +1,100 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from '@react-email/components'
+import * as React from 'react'
+
+interface RegistrationConfirmEmailProps {
+  userFirstname: string
+}
+
+export const RegistrationConfirmEmail = ({
+  userFirstname = 'Zeno',
+}: RegistrationConfirmEmailProps) => (
+  <Html>
+    <Head />
+    <Preview>The sales intelligence platform that helps you uncover qualified leads.</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Img src="/static/next.svg" width="170" height="50" alt="Next" style={logo} />
+        <Text style={paragraph}>Witaj {userFirstname},</Text>
+        <Text style={paragraph}>
+          Konto zostało utworzeone, lecz nie jest jeszcze aktywne. Aby je aktywować, kliknij w
+          poniższy link:
+        </Text>
+        <Section style={btnContainer}>
+          <Button pX={12} pY={12} style={button} href="https://getkoala.com">
+            Aktywuj konto
+          </Button>
+        </Section>
+        <Text style={paragraph}>
+          Jeżeli przycisk nie przekierował Cię automatycznie, skopiuj i wklej poniższy link do
+          przeglądarki:
+        </Text>
+        <Link href="https://getkoala.com">https://getkoala.com</Link>
+        <Text style={paragraph}>
+          Życzymy miłej gry
+          <br />
+          Cash Dynasty Team
+        </Text>
+        <Hr style={hr} />
+        {/*<Text style={footer}>408 Warren Rd - San Mateo, CA 94402</Text>*/}
+      </Container>
+    </Body>
+  </Html>
+)
+
+export default RegistrationConfirmEmail
+
+const main = {
+  backgroundColor: '#ffffff',
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+}
+
+const container = {
+  margin: '0 auto',
+  padding: '20px 0 48px',
+}
+
+const logo = {
+  margin: '0 auto',
+}
+
+const paragraph = {
+  fontSize: '16px',
+  lineHeight: '26px',
+}
+
+const btnContainer = {
+  textAlign: 'center' as const,
+}
+
+const button = {
+  backgroundColor: '#5F51E8',
+  borderRadius: '3px',
+  color: '#fff',
+  fontSize: '16px',
+  textDecoration: 'none',
+  textAlign: 'center' as const,
+  display: 'block',
+}
+
+const hr = {
+  borderColor: '#cccccc',
+  margin: '20px 0',
+}
+
+// const footer = {
+//   color: '#8898aa',
+//   fontSize: '12px',
+// }
