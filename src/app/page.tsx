bb9c3aed54609a3b2fe3345c01d47bf6526f2ee2
Marcin Wiatrak <marcin.wiatrak.95@gmail.com>
2023-10-06 00:10:29 +0200

    update cors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,14 +16,10 @@ const url = process.env.NEXT_PUBLIC_WS_SERVER_URL || 'http://130.162.55.95:3001'
 
 console.log('WS_SERVER_URL', process.env.NEXT_PUBLIC_WS_SERVER_URL)
 
-const socket = io(url, {
-  withCredentials: false,
-})
+const socket = io(url)
 
 export default function Home() {
   const session = useSession()
-
-  console.log(session)
 
   const [sendMail] = useSendConfirmationMailMutation()
   const [message, setMessage] = useState<string[]>([])
@@ -47,10 +43,10 @@ export default function Home() {
   }
 
   useEffect(() => {
-    // socket.on('chat', (data) => {
-    //   console.log('data', data)
-    //   setMessage((prev: string[]) => [...prev, data])
-    // })
+    socket.on('chat', (data) => {
+      console.log('data', data)
+      setMessage((prev: string[]) => [...prev, data])
+    })
 
     getDbMessages()
   }, [])
@@ -64,10 +60,16 @@ export default function Home() {
     setNewMessage('')
   }
 
+  const test = async () => {
+    await axios.post('https://ws.cashdynasty.pl/test').then((res) => {
+      console.log('res', res)
+    })
+  }
+
   return (
     <div className="h-screen bg-slate-700 flex flex-col items-center justify-center gap-5">
       <div className="flex flex-col gap-4 w-full max-w-sm">
-        <Button label="ATAKUJ" />
+        <Button label="ATAKUJ" onClick={test} />
         <Link href="/auth/register">Register nju account</Link>
         <Button
           label="Wyslij email"
