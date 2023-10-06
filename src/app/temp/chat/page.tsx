3d8697b1bca0fd69932264f09154a5edc7b7bfce
Bartosz <demorhul@gmail.com>
2023-10-06 18:32:21 +0200

    Co-authored-by: Marcin Wiatrak <marcin-wiatrak@users.noreply.github.com>

--- src/app/temp/chat/page.tsx
+++ src/app/temp/chat/page.tsx
@@ -1,12 +1,52 @@
 import { Message } from '@molecules'
+import dayjs from 'dayjs'
 
 export default function Chat() {
+  const messageArray = [
+    {
+      id: 1,
+      isMine: true,
+      message:
+        'ttesttesttesttesttesttestttesttesttesttesttesttestttesttesttesttesttesttestttesttesttesttesttesttestttesttesttesttesttesttest ttesttesttesttesttesttestttesttesttesttesttesttest',
+      createdAt: dayjs().format('HH:mm DD.MM.YYYY'),
+      author: 'Vezo',
+    },
+    {
+      id: 2,
+      isMine: false,
+      message: 'false',
+      createdAt: dayjs().format('HH:mm DD.MM.YYYY'),
+      author: 'MarcinW',
+    },
+    {
+      id: 3,
+      isMine: true,
+      message: 'test',
+      createdAt: dayjs().format('HH:mm DD.MM.YYYY'),
+      author: 'Vezo',
+    },
+    {
+      id: 4,
+      isMine: false,
+      message: 'false',
+      createdAt: dayjs().format('HH:mm DD.MM.YYYY'),
+      author: 'MarcinW',
+    },
+  ]
   return (
     <div className="h-screen bg-slate-700 flex flex-col items-center justify-center gap-5">
       <h1>Chat</h1>
-      <div className="flex flex-col gap-4 w-full max-w-sm bg-slate-300">
-        <div className="m-5">
-          <Message />
+      <div className="flex flex-col gap-4 w-full max-w-sm ">
+        <div>
+          {messageArray.map((item) => (
+            <Message
+              key={item.id}
+              isMine={item.isMine}
+              message={item.message}
+              createdAt={item.createdAt}
+              author={item.author}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/molecules/Message/Message.tsx
+++ src/components/molecules/Message/Message.tsx
@@ -1,3 +1,15 @@
-export const Message = () => {
-  return <h1>dupa </h1>
+import { cn } from '@/utils/styles'
+
+export const Message = ({ isMine, message, createdAt, author }) => {
+  return (
+    <div
+      className={cn('p-2 bg-slate-400 mt-2 rounded-md', {
+        ['bg-slate-200 text-right']: isMine,
+      })}
+    >
+      <p className="text-xs">{createdAt}</p>
+      <p className="text-l font-bold">{author}</p>
+      <p className="break-words">{message}</p>
+    </div>
+  )
 }
