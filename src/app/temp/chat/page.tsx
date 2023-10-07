b66f29c6b75013254dadff61f5e3cd141d6375d8
Bartosz Jaworowicz <68512578+Demorhul@users.noreply.github.com>
2023-10-07 09:28:45 +0200

    CAS-28 Create temp chat page (#25)

---
+++ src/app/temp/chat/page.tsx
@@ -0,0 +1,9 @@
+export default function Chat() {
+  return (
+    <div className="h-screen bg-slate-700 flex flex-col items-center justify-center gap-5">
+      <div className="flex flex-col gap-4 w-full max-w-sm">
+        <h1>Chat</h1>
+      </div>
+    </div>
+  )
+}
