af35f44e081ff25ecf6734543c7048ea1cc8e093
Dominika Kazmierczak <dominika@enabler.no>
2023-10-07 23:54:03 +0200

    CAS-35 Added comment

--- src/components/organisms/Form/Form.tsx
+++ src/components/organisms/Form/Form.tsx
@@ -5,10 +5,11 @@ type FormProps = {
 } & React.DetailedHTMLProps<React.FormHTMLAttributes<HTMLFormElement>, HTMLFormElement>
 
 const Form = ({ children, ...props }: FormProps) => {
+  // chce zeby form mial "max-w-xl" klase proszeeeeeeee - Domi <333333
   return (
-    <div className="flex flex-col gap-4 bg-slate-800 p-12 w-full max-w-xl">
-      <form {...props}>{children}</form>
-    </div>
+    <form {...props}>
+      <div className="flex flex-col gap-4 bg-slate-800 p-12 w-full">{children}</div>
+    </form>
   )
 }
 
