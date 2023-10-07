7b190e4565034610008ef140343ad70f1fcca18a
Dominika Kazmierczak <dominika@enabler.no>
2023-10-07 21:15:04 +0200

    Changed login form styles

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,6 +3,8 @@ import { TextInput } from '@/components/atoms/TextInput'
 import { useState } from 'react'
 import { Button } from '@/components/atoms/Button'
 import { signIn } from 'next-auth/react'
+import Form from '@/components/organisms/Form/Form'
+import Link from 'next/link'
 
 export default function Login() {
   const [username, setUsernane] = useState<string | undefined>('')
@@ -11,23 +13,29 @@ export default function Login() {
   const handleLogin = async () => await signIn('credentials', { username, password })
 
   return (
-    <div className="h-screen bg-slate-700 flex flex-col gap-5">
-      <TextInput
-        leftIcon
-        placeholder="Username"
-        type="text"
-        value={username}
-        onChange={(e) => setUsernane(e.target.value)}
-      />
-      <TextInput
-        leftIcon
-        placeholder="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <Button label="Zaloguj" onClick={handleLogin} />
+    <div className="h-full flex items-center justify-center">
+      <Form>
+        <TextInput
+          leftIcon
+          fullWidth
+          placeholder="Username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsernane(e.target.value)}
+        />
+        <TextInput
+          leftIcon
+          fullWidth
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button label="Zaloguj" onClick={handleLogin} />
+        <Link href="/auth/register" className="cursor-pointer">
+          <div className="flex justify-center text-white">Nie masz konta? Zarejestruj sie!</div>
+        </Link>
+      </Form>
     </div>
   )
 }
