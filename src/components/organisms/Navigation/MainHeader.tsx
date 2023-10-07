67b6c75fff563d41496f2d6863f426bd892db5e2
Dominika Kazmierczak <dominika@enabler.no>
2023-10-07 22:08:56 +0200

    CAS-35 Added userid

--- src/components/organisms/Navigation/MainHeader.tsx
+++ src/components/organisms/Navigation/MainHeader.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@/components/atoms'
+import { Button, UserId } from '@/components/atoms'
 import Link from 'next/link'
 
 export const MainHeader = () => {
   return (
     <div className="absolute top-0 right-0 flex gap-1">
-      {/* <UserId /> */}
+      <UserId />
       <Link href="/">
         <Button label="Home" />
       </Link>
