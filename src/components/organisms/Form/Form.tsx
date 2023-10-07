be3322f62717b46d2381e747ee542928f24e0f44
Dominika Kazmierczak <dominika@enabler.no>
2023-10-07 23:26:56 +0200

    CAS-35 Refactored form

--- src/components/organisms/Form/Form.tsx
+++ src/components/organisms/Form/Form.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-interface Props {
+interface FormProps {
   children: React.ReactNode
 }
 
-const Form = ({ children }: Props) => {
-  return <div className="flex flex-col gap-4 bg-slate-800 p-12 w-full max-w-xl">{children}</div>
+const Form = ({ children, ...props }: FormProps) => {
+  return (
+    <div className="flex flex-col gap-4 bg-slate-800 p-12 w-full max-w-xl">
+      <form {...props}>{children}</form>
+    </div>
+  )
 }
 
 export default Form
