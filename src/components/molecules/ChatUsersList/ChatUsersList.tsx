cd5a2ddd6003b42d325d9096d38aef31e367a2f9
Adrian <adrian.gorniak97@op.pl>
2023-10-11 20:01:46 +0200

    CAS-29 adding a fucnction with error

--- src/components/molecules/ChatUsersList/ChatUsersList.tsx
+++ src/components/molecules/ChatUsersList/ChatUsersList.tsx
@@ -3,11 +3,13 @@ import { ChatUsersListItem } from './parts'
 import { useGetUsersQuery } from '@/api'
 
 export const ChatUsersList = () => {
-  const usersList = useGetUsersQuery()
+  const { data, isError } = useGetUsersQuery()
 
   const liStyle = 'w-1/3 bg-gray-200 text-center'
 
-  console.log(usersList.data?.usersList)
+  if (isError) {
+    return <div>Error</div>
+  }
   return (
     <div className="w-[300px] bg-gray-400 p-2 flex flex-col justify-center ">
       <h2 className="py-2">Chat users list</h2>
@@ -17,7 +19,7 @@ export const ChatUsersList = () => {
           <li className={liStyle}>Friends</li>
         </ul>
       </div>
-      {usersList.data?.usersList.map((user) => <ChatUsersListItem user={user} key={user?.id} />)}
+      {data?.usersList.map((user) => <ChatUsersListItem user={user} key={user?.id} />)}
     </div>
   )
 }
