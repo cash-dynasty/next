d774d40db8e72bf3152549ddd31dd99d2554a8ec
Dominika Kazmierczak <dominika@enabler.no>
2023-10-16 20:23:29 +0200

    CAS-43 Removed unused code

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -3,7 +3,6 @@ import { RegisterForm } from '@organisms'
 export default function Register() {
   return (
     <div className="w-full flex justify-center lg:justify-start items-center">
-      <div className="absolute flex justify-center items-center overflow-hidden h-full w-full"></div>
       <div
         className="bg-no-repeat bg-center bg-cover w-full min-h-screen absolute top-0 left-0 z-0"
         style={{ backgroundImage: 'url(/city.png)' }}
