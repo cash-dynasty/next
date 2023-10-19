c17932e08368ebf1bc6e867604d8b7ed0d709380
Dominika Kazmierczak <dominika@enabler.no>
2023-10-19 08:17:14 +0200

    CAS-43 added self closing tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div
         className="bg-no-repeat bg-center bg-cover w-full min-h-screen absolute top-0 left-0 z-0"
         style={{ backgroundImage: 'url(/city.png)' }}
-      ></div>
+      />
       <div className="flex flex-col gap-4 w-full max-w-xl p-2 lg:p-0 lg:ml-20">
         {state === 'login' ? (
           <LoginForm onClick={() => setState('registration')} />
