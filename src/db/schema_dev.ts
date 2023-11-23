98acf82711765edd6adde2d22b6a30ca8f603656
Dominika Kazmierczak <dominika@enabler.no>
2023-11-23 17:56:31 +0100

    Fixed something

--- src/db/schema_dev.ts
+++ src/db/schema_dev.ts
@@ -1,21 +1,33 @@
-import { pgEnum, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+import {
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  serial,
+  varchar,
+  boolean,
+  integer,
+} from 'drizzle-orm/pg-core'
 
 export const role = pgEnum('Role', ['USER', 'ADMIN', 'MODERATOR'])
 
 export const user = pgTable('User', {
-  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  id: serial('id').primaryKey().notNull(),
   email: text('email').notNull(),
-  password: text('password').notNull(),
+  password: varchar('password', { length: 256 }).notNull(),
   role: role('role').default('USER').notNull(),
+  isActive: boolean('isActive').default(false).notNull(),
   createdAt: timestamp('createdAt', { precision: 3, mode: 'string' }).defaultNow().notNull(),
+  updatedAt: timestamp('updatedAt', { precision: 3, mode: 'string' }).notNull(),
 })
 
-// export const userRelations = relations(user, ({ one }) => ({
-//   player: one(player),
-// }))
+export const userRelations = relations(user, ({ one }) => ({
+  player: one(player),
+}))
 
 export const player = pgTable('Player', {
-  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  id: serial('id').primaryKey().notNull(),
   nickname: text('nickname').notNull(),
-  userId: uuid('user_id').references(() => user.id),
+  userId: integer('user_id').references(() => user.id),
 })
