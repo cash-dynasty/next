7b8153eed4c83659f6f5dd00ab4c04df98800e10
Marcin Wiatrak <marcin.wiatrak.95@gmail.com>
2023-12-03 23:42:18 +0100

    CAS-64 Animated value change component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { AuthContainer } from '@organisms'
+import { CurrentMoneyIndicator } from '@molecules'
 
 export default function Home() {
   return (
     <div className="w-full min-h-screen flex justify-center lg:justify-start items-center">
       <div
-        className="bg-no-repeat bg-center bg-cover w-full min-h-screen absolute top-0 left-0 z-0"
+        className="bg-no-repeat bg-center bg-cover w-full min-h-screen absolute top-0 left-0 -z-10"
         style={{ backgroundImage: 'url(/city.png)' }}
       />
       <AuthContainer />
+      <CurrentMoneyIndicator />
     </div>
   )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { Button, TextInput, UserId, Typography } from './atoms'
-export { Message, ChatUsersList } from './molecules'
+export { Message, ChatUsersList, CurrentMoneyIndicator } from './molecules'
 export { LoginForm, RegisterForm, Nav, NicknameContent, SectorSelectContent } from './organisms'
