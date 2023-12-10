f0623231234948a2ae541b916ee11ba7d5c8dd01
Dominika Kazmierczak <dominika@enabler.no>
2023-12-10 20:30:40 +0100

    CAS-73 Created safebox component

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -15,6 +15,8 @@ import { relations } from 'drizzle-orm'
 export const role = pgEnum('Role', ['USER', 'ADMIN', 'MODERATOR'])
 export const sector = pgEnum('Sector', ['IT', 'MEDIC', 'FINANCE', 'ARMY'])
 
+export type ESector = typeof sector.enumValues
+
 export const user = pgTable('user', {
   id: serial('id').primaryKey(),
   email: text('email').notNull(),
@@ -24,6 +26,9 @@ export const user = pgTable('user', {
   createdAt: timestamp('createdAt', { precision: 3, mode: 'string' }).defaultNow().notNull(),
   updatedAt: timestamp('updatedAt', { precision: 3, mode: 'string' }).defaultNow().notNull(),
 })
+
+export type TUserSelect = typeof user.$inferSelect
+export type TUserInsert = typeof user.$inferInsert
 
 export const userRelations = relations(user, ({ one, many }) => ({
   player: one(player, {
@@ -80,11 +85,17 @@ export const property = pgTable('property', {
   safeboxAmount: integer('safebox_amount').default(0).notNull(),
 })
 
-export const propertyRelations = relations(property, ({ one }) => ({
+export type TPropertySelect = typeof property.$inferSelect & {
+  owner?: TUserSelect
+  buildings?: TBuildingsSelect[]
+}
+
+export const propertyRelations = relations(property, ({ one, many }) => ({
   owner: one(player, {
     fields: [property.ownerId],
     references: [player.id],
   }),
+  buildings: many(building),
 }))
 
 export const building = pgTable('building', {
@@ -94,7 +105,7 @@ export const building = pgTable('building', {
   configBuildingId: integer('config_building_id').notNull(),
 })
 
-export type TBuildingsSelect = typeof building.$inferSelect
+export type TBuildingsSelect = typeof building.$inferSelect & { configBuilding: TCBuildingSelect }
 
 export const buildingRelations = relations(building, ({ one }) => ({
   property: one(property, {
@@ -109,25 +120,33 @@ export const buildingRelations = relations(building, ({ one }) => ({
 
 export const cBuilding = pgTable('c_building', {
   id: serial('id').primaryKey(),
-  name: text('name').notNull(),
+  codeName: text('code_name').default('building').notNull(),
+  name: text('name'),
   description: text('description'),
   sector: sector('sector').notNull(),
   maxLevel: integer('max_level').notNull(),
 })
 
+export type TCBuildingSelect = typeof cBuilding.$inferSelect & {
+  buildingUpgradeRequirement?: TCBuildingUpgradeRequirementSelect
+}
+export type TCBuildingInsert = typeof cBuilding.$inferInsert
+
 export const cBuildingRelations = relations(cBuilding, ({ many }) => ({
   buildingUpgradeRequirement: many(cBuildingUpgradeRequirement),
-  requiredBuilding: many(cRequiredBuilding, {
-    relationName: 'requiredBuilding',
-  }),
 }))
 
 export const cBuildingUpgradeRequirement = pgTable('c_building_upgrade_requirement', {
   id: serial('id').primaryKey(),
   level: integer('level').notNull(),
   upgradePrice: integer('upgrade_price').notNull(),
+  upgradeDuration: integer('upgrade_duration').default(10).notNull(),
   buildingId: integer('building_id').notNull(),
 })
+export type TCBuildingUpgradeRequirementSelect = typeof cBuildingUpgradeRequirement.$inferSelect & {
+  requiredBuilding?: TCRequiredBuildingSelect
+}
+export type TCBuildingUpgradeRequirementInsert = typeof cBuildingUpgradeRequirement.$inferInsert
 
 export const cBuildingUpgradeRequirementRelations = relations(
   cBuildingUpgradeRequirement,
@@ -142,19 +161,18 @@ export const cBuildingUpgradeRequirementRelations = relations(
 export const cRequiredBuilding = pgTable('c_required_building', {
   id: serial('id').primaryKey(),
   buildingId: integer('building_id').notNull(),
-  requiredBuildingId: integer('required_building_id').notNull(),
   requiredBuildingLevel: integer('required_building_level').notNull(),
+  buildingUpgradeRequirementId: integer('building_upgrade_requirement_id').notNull(),
 })
+export type TCRequiredBuildingSelect = typeof cBuildingUpgradeRequirement.$inferSelect & {
+  building?: TCBuildingSelect
+}
+export type TCRequiredBuildingInsert = typeof cBuildingUpgradeRequirement.$inferInsert
 
 export const cRequiredBuildingRelations = relations(cRequiredBuilding, ({ one }) => ({
   building: one(cBuilding, {
     relationName: 'building',
     fields: [cRequiredBuilding.buildingId],
-    references: [cBuilding.id],
-  }),
-  requiredBuilding: one(cBuilding, {
-    relationName: 'requiredBuilding',
-    fields: [cRequiredBuilding.requiredBuildingId],
     references: [cBuilding.id],
   }),
 }))
